77f27a333440165253326060b1127b72671fc79d
mkharazishvili95 <mkharazishvili1@gmail.com>
2023-11-22 15:45:23 +0400

    strings

---
+++ WorkingWithStrings/CopyingStrings.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace WorkingWithStrings
+{
+    public static class CopyingStrings
+    {
+        /// <summary>
+        /// Copies one character from the <paramref name="source"/> to the <paramref name="destination"/>.
+        /// </summary>
+        public static string CopyOneChar(string source, string destination)
+        {
+            char[] destinationArray = destination.ToCharArray();
+
+            source.CopyTo(0, destinationArray, 4, source.Length);
+            return new string(destinationArray);
+        }
+
+        /// <summary>
+        /// Copies three characters from the <paramref name="source"/> to the <paramref name="destination"/>.
+        /// </summary>
+        public static string CopyThreeChars(string source, string destination)
+        {
+            // TODO #8-1. Analyze unit tests for the method, and add the method implementation.
+            // Use String.CopyTo method to copy characters: https://docs.microsoft.com/en-us/dotnet/api/system.string.copyto
+            char[] destinationArray = destination.ToCharArray();
+            source.CopyTo(0, destinationArray, 0, Math.Min(3, source.Length));
+            return new string(destinationArray);
+        }
+
+        /// <summary>
+        /// Copies five characters from the <paramref name="source"/> to the <paramref name="destination"/>.
+        /// </summary>
+        public static string CopyFiveChars(string source, string destination)
+        {
+            // TODO #8-2. Analyze unit tests for the method, and add the method implementation.
+            char[] destinationArray = destination.ToCharArray();
+            int startIndex = destination.IndexOf('*', StringComparison.Ordinal);
+            source.CopyTo(0, destinationArray, startIndex, source.Length);
+            return new string(destinationArray);
+        }
+
+        /// <summary>
+        /// Copies six characters from the <paramref name="source"/> to the <paramref name="destination"/>.
+        /// </summary>
+        public static string CopySixChars(string source, string destination)
+        {
+            // TODO #8-3. Analyze unit tests for the method, and add the method implementation.
+            char[] destinationArray = destination.ToCharArray();
+            source.CopyTo(2, destinationArray, 5, 6);
+            return new string(destinationArray);
+        }
+
+        /// <summary>
+        /// Gets a production code by copying substrings of the <paramref name="regionCode"/>, <paramref name="locationCode"/>, <paramref name="dateCode"/> and <paramref name="factoryCode"/> parameters to the <paramref name="template"/>.
+        /// </summary>
+        public static string GetProductionCode(string template, string regionCode, string locationCode, string dateCode, string factoryCode)
+        {
+            // TODO #8-4. Analyze unit tests for the method, and add the method implementation.
+            // Fixing multiple consecutive whitespaces
+            char[] templateArray = template.ToCharArray();
+            regionCode.CopyTo(1, templateArray, 0, 1);
+            locationCode.CopyTo(4, templateArray, 3, 2);
+            dateCode.CopyTo(3, templateArray, 7, 3);
+            factoryCode.CopyTo(2, templateArray, 12, 4);
+            return new string(templateArray);
+        }
+    }
+}
---
+++ WorkingWithStrings/SplittingStrings.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Linq;
+
+namespace WorkingWithStrings
+{
+    public static class SplittingStrings
+    {
+        /// <summary>
+        /// Splits a comma-separated string into substrings that are based on the comma character, and return an array whose elements contain the substrings.
+        /// </summary>
+        public static string[] SplitCommaSeparatedString(string str)
+        {
+            return str.Split(',');
+        }
+
+        /// <summary>
+        /// Splits a colon-separated string into substrings that are based on the colon character, and return an array whose elements contain the substrings.
+        /// </summary>
+        public static string[] SplitColonSeparatedString(string str)
+        {
+            // TODO #5-1. Analyze unit tests for the method, and add the method implementation.
+            // Use String.Split method: https://docs.microsoft.com/en-us/dotnet/api/system.string.split
+            return str.Split(':');
+        }
+
+        /// <summary>
+        /// Splits a comma-separated string into substrings that are based on the comma character, and return an array whose elements contain the substrings.
+        /// </summary>
+        public static string[] SplitCommaSeparatedStringMaxTwoElements(string str)
+        {
+            // TODO #5-2. Analyze unit tests for the method, and add the method implementation.
+            // Use String.Split method: https://docs.microsoft.com/en-us/dotnet/api/system.string.split
+            string[] result = str.Split(new string[] { "," }, 2, StringSplitOptions.None);
+            return result;
+        }
+
+        /// <summary>
+        /// Splits a colon-separated string into substrings that are based on the colon character, and return an array whose elements contain the substrings.
+        /// </summary>
+        public static string[] SplitColonSeparatedStringMaxThreeElements(string str)
+        {
+            // TODO #5-3. Analyze unit tests for the method, and add the method implementation.
+            // Use String.Split method: https://docs.microsoft.com/en-us/dotnet/api/system.string.split
+            string[] result = str.Split(new string[] { ":" }, 3, StringSplitOptions.None);
+            return result;
+        }
+
+        /// <summary>
+        /// Splits a hyphen-separated string into substrings that are based on the hyphen character, and return an array whose elements contain the substrings.
+        /// </summary>
+        public static string[] SplitHyphenSeparatedStringMaxThreeElementsRemoveEmptyStrings(string str)
+        {
+            // TODO #5-4. Analyze unit tests for the method, and add the method implementation.
+            // Use String.Split method: https://docs.microsoft.com/en-us/dotnet/api/system.string.split
+            string[] result = str.Split(new[] { '-' }, 3, StringSplitOptions.RemoveEmptyEntries);
+            return result;
+        }
+
+        /// <summary>
+        /// Splits a separated string that is separated with colon and comma characters into substrings, and return an array whose elements contain the substrings.
+        /// </summary>
+        public static string[] SplitColonAndCommaSeparatedStringMaxFourElementsRemoveEmptyStrings(string str)
+        {
+            // TODO #5-5. Analyze unit tests for the method, and add the method implementation.
+            // Use String.Split method: https://docs.microsoft.com/en-us/dotnet/api/system.string.split
+            string[] result = str.Split(new[] { ':', ',' }, 4, StringSplitOptions.RemoveEmptyEntries);
+            return result;
+        }
+
+        /// <summary>
+        /// Splits a sentence into substrings, and return an array whose elements contain only words.
+        /// </summary>
+        public static string[] GetOnlyWords(string str)
+        {
+            // TODO #5-6. Analyze unit tests for the method, and add the method implementation.
+            // Use String.Split method: https://docs.microsoft.com/en-us/dotnet/api/system.string.split
+            char[] separators = new char[] { ' ', '\t', '\n', '\r', '.', ',', ';', ':', '-', '!', '?' };
+            string[] words = str.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            return words;
+        }
+
+        /// <summary>
+        /// Splits a CSV (comma-separated values) string into substrings that are based on the comma character, and return an array of the CSV line elements.
+        /// </summary>
+        public static string[] GetDataFromCsvLine(string str)
+        {
+            // TODO #5-7. Analyze unit tests for the method, and add the method implementation.
+            // Use String.Split method: https://docs.microsoft.com/en-us/dotnet/api/system.string.split
+            string[] data = str.Split(',');
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = data[i].Trim();
+            }
+
+            return data;
+        }
+    }
+}
